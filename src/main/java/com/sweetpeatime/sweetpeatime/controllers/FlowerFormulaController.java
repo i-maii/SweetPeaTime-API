635a059002817b0aefe2c79953710d0f4e31ce31
Mac <pattrara.pj@gmail.com>
2021-02-21 22:05:16 +0700

    no message

--- src/main/java/com/sweetpeatime/sweetpeatime/controllers/FlowerFormulaController.java
+++ src/main/java/com/sweetpeatime/sweetpeatime/controllers/FlowerFormulaController.java
@@ -33,4 +33,60 @@ public class FlowerFormulaController {
         }
         return flowerQuantityAvaliableDtos;
     }
+
+    @GetMapping(value="/searchFlowerFormula")
+    public List<FlowerFormula> searchFlowerFormula() {
+        return this.flowerFormulaRepository.findAll();
+    }
+
+    @PostMapping(value="/search")
+    public List<FlowerFormula> search(
+            @RequestParam(value = "name", required = false) String name,
+            @RequestParam(value = "pattern", required = false) String pattern,
+            @RequestParam(value = "occasion", required = false) String occasion,
+            @RequestParam(value = "price", required = false) Number price,
+            @RequestParam(value = "quantityAvailable", required = false) String quantityAvailable,
+            @RequestParam(value = "size", required = false) String size
+    ){
+
+        StringBuffer selectQueryStr = new StringBuffer("SELECT * FROM FlowerFormula f WHERE 1 = 1 ");
+       // StringBuffer countQueryStr = new StringBuffer("SELECT count(c.id) FROM FlowerFormula f WHERE 1 = 1 ");
+
+        if (name != null) {
+            selectQueryStr.append("AND f.name = :name ");
+
+        } else {
+            //selectQueryStr.append("AND c.name is null ");
+           // countQueryStr.append("AND c.name is null ");
+        }
+
+        if(pattern != null){
+            selectQueryStr.append("AND f.pattern = :pattern ");
+
+        }
+
+        if(occasion != null){
+            selectQueryStr.append("AND f.occasion = :occasion ");
+
+        }
+
+        if(price != null){
+            selectQueryStr.append("AND f.price <= :price ");
+
+        }
+
+        if(quantityAvailable != null){
+            selectQueryStr.append("AND f.quantityAvailable >= :quantityAvailable ");
+
+        }
+
+        if(size != null) {
+            selectQueryStr.append("AND f.size = :size ");
+        }
+
+        selectQueryStr.append("ORDER BY f.name DESC ").toString();
+        return this.flowerFormulaRepository.searchFlowerFormula(selectQueryStr);
+
+    }
+
 }
--- src/main/java/com/sweetpeatime/sweetpeatime/repositories/FlowerFormulaRepository.java
+++ src/main/java/com/sweetpeatime/sweetpeatime/repositories/FlowerFormulaRepository.java
@@ -4,9 +4,15 @@ import com.sweetpeatime.sweetpeatime.entities.FlowerFormula;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface FlowerFormulaRepository extends JpaRepository<FlowerFormula, Integer> {
 
     @Query(value = "SELECT quantityAvailable FROM FlowerFormula ff WHERE ff.id = ?1",
             nativeQuery = true)
     Integer getQuantityAvailable(Integer id);
+
+    @Query(value = query,
+            nativeQuery = true)
+    List<FlowerFormula> searchFlowerFormula(StringBuffer query);
 }
